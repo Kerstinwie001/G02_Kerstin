8d0d9cbf2432f0e2806d95b1d96613d5e35331f2
Kerstinwie001 <kerstin.wieczorek@stud.unibas.ch>
2024-09-13 13:16:33 +0200

    Qualitycheck

--- analysis.R
+++ analysis.R
@@ -1,1 +1,6 @@
 #analysis
+mydata<-readLines("C:/Users/Kerstin/Desktop/gittest/G02_Kerstin/mydata.txt, header= TRUE, sep="/t"")
+mydata <- read.delim("C:/Users/Kerstin/Desktop/gittest/G02_Kerstin/mydata.txt", header = TRUE, sep = "\t")
+filtered_data <- mydata[mydata$Filter != 1, ]
+remaining_participants <- nrow(filtered_data)
+print(remaining_participants)
