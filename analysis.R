d8a4a3939e16148db74222aa6b499399e625a09f
Kerstinwie001 <kerstin.wieczorek@stud.unibas.ch>
2024-09-13 13:21:33 +0200

    testosteron values

--- analysis.R
+++ analysis.R
@@ -4,3 +4,5 @@ mydata <- read.delim("C:/Users/Kerstin/Desktop/gittest/G02_Kerstin/mydata.txt", 
 filtered_data <- mydata[mydata$Filter != 1, ]
 remaining_participants <- nrow(filtered_data)
 print(remaining_participants)
+testosterone_summary <- aggregate(Testosteron ~ Sex, data = mydata, summary)
+print(testosterone_summary)
